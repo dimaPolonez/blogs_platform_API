e70b2d8f61f0fb59eb63e43a936463733b57d3c1
dimaPolonez <dima-barxat@yandex.ru>
2022-12-14 16:28:41 +0300

    feat: add modules

--- src/data/blogs.data.ts
+++ src/data/blogs.data.ts
@@ -1,5 +1,12 @@
-export let BLOGS = [
-    {
+// @ts-ignore
+import {BlogsFieldsType} from "../models/data.models";
 
+
+export let BLOGS: Array<BlogsFieldsType> = [
+    {
+        id: "2020",
+        name: "Dima",
+        description: "asdasd",
+        websiteUrl: "asd",
     }
 ]
---
+++ src/models/data.models.ts
@@ -0,0 +1,6 @@
+export type BlogsFieldsType = {
+    id: string
+    name: string
+    description: string
+    websiteUrl: string
+}
