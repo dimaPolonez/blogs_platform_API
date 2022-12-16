b4f5d3335fc5b16606573a96fe8b0ba5b7b9435d
dimaPolonez <105782493+dimaPolonez@users.noreply.github.com>
2022-12-16 12:58:44 +0300

    feat: add request models

---
+++ src/models/request.models.ts
@@ -0,0 +1,6 @@
+import { Request } from 'express';
+
+export type RequestBody<T> = Request<{}, {}, T>;
+export type RequestQuery<T> = Request<{}, {}, {}, T>;
+export type RequestParams<T> = Request<T>;
+export type RequestParamsAndBody<P, B> = Request<P, B>;
