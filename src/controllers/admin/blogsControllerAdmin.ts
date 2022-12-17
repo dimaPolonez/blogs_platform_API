e6f69e2aa7831f31ec4b747d048a4f9a0f76e9cb
dimaPolonez <dima-barxat@yandex.ru>
2022-12-17 21:43:24 +0300

    fix: errors validator

--- src/controllers/admin/blogsControllerAdmin.ts
+++ src/controllers/admin/blogsControllerAdmin.ts
@@ -14,10 +14,11 @@ blogsRouterAdmin.post(
     '/',indexMiddleware.BLOGS_VALIDATOR,indexMiddleware.ERRORS_VALIDATOR,
     (req: Request, res: Response) => {
 
-        let result = postBlogFunc(req.body);
+        let result: object = postBlogFunc(req.body);
 
-        res.json(result).sendStatus(ERRORS_CODE.CREATED_201);
-
+        res
+            .json(result)
+            .sendStatus(ERRORS_CODE.CREATED_201);
     }
 );
 
--- src/middleware/index.middleware.ts
+++ src/middleware/index.middleware.ts
@@ -1,7 +1,7 @@
 import {parserMiddleware} from "./json.middlware";
 import {blogsValidator} from "./blogs.middleware";
 import {NextFunction, Request, Response} from "express";
-import {validationResult} from "express-validator";
+import {param, validationResult} from "express-validator";
 import {postsValidator} from "./posts.middleware";
 
 
@@ -12,7 +12,10 @@ export const errorsValidator = (req: Request, res: Response, next: NextFunction)
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errorsMessages: errors.array().map(
-                (x) => x.msg)});
+                (x) => {
+                    let object: object = {message: x.msg,field: x.param}
+                    return object
+                })});
     } else {
         next()
     }
