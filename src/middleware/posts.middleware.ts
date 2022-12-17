ee8a3244e1644b2bc5ea6252be3c80086185f314
dimaPolonez <dima-barxat@yandex.ru>
2022-12-17 22:14:09 +0300

    fix: body validator blogid

--- src/middleware/posts.middleware.ts
+++ src/middleware/posts.middleware.ts
@@ -4,6 +4,6 @@ export const postsValidator = [
     body('title').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 30}).bail().withMessage('Field title incorrect'),
     body('shortDescription').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 100}).bail().withMessage('Field shortDescription incorrect'),
     body('content').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 1000}).bail().withMessage('Field content incorrect'),
-    body('blogId').isString().bail().trim().bail().notEmpty().bail().withMessage('Field blogId incorrect')
+    body('blogId').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 15}).bail().withMessage('Field blogId incorrect')
 ]
 
