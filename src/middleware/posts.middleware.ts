67586aeba80bdbfddb8d28a10417993d9247cad0
dimaPolonez <dima-barxat@yandex.ru>
2022-12-17 22:00:11 +0300

    fix: post content

--- src/middleware/posts.middleware.ts
+++ src/middleware/posts.middleware.ts
@@ -3,7 +3,7 @@ import {body} from "express-validator";
 export const postsValidator = [
     body('title').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 30}).bail().withMessage('Field title incorrect'),
     body('shortDescription').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 100}).bail().withMessage('Field shortDescription incorrect'),
-    body('content').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 1000}).bail().isURL().bail().withMessage('Field content incorrect'),
+    body('content').isString().bail().trim().bail().notEmpty().bail().isLength({ max: 1000}).bail().withMessage('Field content incorrect'),
     body('blogId').isString().bail().trim().bail().notEmpty().bail().withMessage('Field blogId incorrect')
 ]
 
