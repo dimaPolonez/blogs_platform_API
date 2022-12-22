d6d62604e472e5f61f6934212e5ce44aad4ab20d
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 11:42:00 +0300

    reload #1 index + server

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -2,9 +2,5 @@ import { Request, Response } from 'express';
 import { app } from '../index';
 
 app.get('/', (req: Request, res: Response) => {
-  try {
-    res.status(200).json('Server start!');
-  } catch (e) {
-    res.status(500).json(e);
-  }
+  res.sendStatus(200);
 });
