d73d5e74c00f98b8c41f4449d179ec5bc3cea51b
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 11:39:47 +0300

    fix: reload

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 import express from 'express';
-import { startBD } from './data/db';
-
-const PORT = process.env.PORT || 5000;
 
 export const app = express();
+const port = process.env.PORT || 3001;
 
-async function startApp() {
-  try {
-    await startBD();
-    app.listen(PORT, () => {
-      console.log(`Example app listening on port ${PORT}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-startApp();
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,21 +1,10 @@
 import { Request, Response } from 'express';
-import { indexMiddleware } from '../middleware/index.middleware';
-import blogRouter from './blog.router';
-import postRouter from './post.router';
-import testingRouter from './testing.router';
-import { ERRORS_CODE } from '../data/errors.data';
-import {app} from "../index";
-
-
-app.use(indexMiddleware.JSON_PARSER);
-app.use('/blogs', blogRouter);
-app.use('/posts', postRouter);
-app.use('/testing/all-data', testingRouter);
+import { app } from '../index';
 
 app.get('/', (req: Request, res: Response) => {
   try {
-    res.status(ERRORS_CODE.OK_200).json('Server start!');
+    res.status(200).json('Server start!');
   } catch (e) {
-    res.status(ERRORS_CODE.INTERNAL_SERVER_ERROR_500).json(e);
+    res.status(500).json(e);
   }
 });
