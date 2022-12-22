0b94f82de21497c53994b46e9f51d704a9c2ea3b
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 12:23:46 +0300

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 export const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('Hello, server start!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
