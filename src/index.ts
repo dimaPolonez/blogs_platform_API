7de1d2b8b74765dcbfea7fe375876f22e2f02ebf
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 15:00:35 +0300

    Modifed congifs

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import express, {Request, Response} from 'express'
-import {startBD} from "./data/db.data";
-import blogRouter from "./routes/blog.router";
-import postRouter from "./routes/post.router";
-import {allDeleteBase} from "./services/index.service";
-import {ERRORS_CODE} from "./data/errors.data";
-
+import express, { Request, Response } from 'express';
+import { startBD } from './data/db.data';
+import blogRouter from './routes/blog.router';
+import postRouter from './routes/post.router';
+import { allDeleteBase } from './services/index.service';
+import { ERRORS_CODE } from './data/errors.data';
 
 const PORT = process.env.PORT || 5000;
 
 export const app = express();
 
 async function startApp() {
-    try {
-        await startBD();
-        app.listen(PORT, () => {
-            console.log(`Example app listening on port ${PORT}`);
-        });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await startBD();
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 startApp();
@@ -29,12 +28,10 @@ app.use('/blogs', blogRouter);
 app.use('/posts', postRouter);
 
 app.get('/', (req: Request, res: Response) => {
-    res.json('Server start!')
-})
+  res.json('Server start!');
+});
 
-app.delete(
-    '/testing/all-data', (req: Request, res: Response) => {
-        allDeleteBase();
-        res.status(ERRORS_CODE.NO_CONTENT_204);
-    }
-);
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+  allDeleteBase();
+  res.status(ERRORS_CODE.NO_CONTENT_204);
+});
