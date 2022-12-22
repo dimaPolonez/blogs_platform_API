2322777702b5953674f847ff464ad25c452b951e
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 09:33:07 +0300

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { startBD } from './data/db';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 export const app = express();
 
