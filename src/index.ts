dc4ee86d9e55787227dd76f16f6f239bb39bcb94
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 12:55:53 +0300

    server run

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.json('Hello, server start!');
+  res.json('Server start!');
 });
 
 app.listen(port, () => {
