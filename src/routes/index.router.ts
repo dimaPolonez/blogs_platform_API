4c7611d78b64d38ce769240b05ba1d21e1001e27
dimaPolonez <dima-barxat@yandex.ru>
2022-12-22 09:07:13 +0300

    fix bags

--- src/routes/index.router.ts
+++ src/routes/index.router.ts
@@ -1,9 +1,9 @@
 import { app } from '../index';
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { indexMiddleware } from '../middleware/index.middleware';
-import blogRouter from "./blog.router";
-import postRouter from "./post.router";
-import testingRouter from "./testing.router";
+import blogRouter from './blog.router';
+import postRouter from './post.router';
+import testingRouter from './testing.router';
 import { ERRORS_CODE } from '../data/errors.data';
 
 app.use(indexMiddleware.JSON_PARSER);
@@ -11,9 +11,10 @@ app.use('/blogs', blogRouter);
 app.use('/posts', postRouter);
 app.use('/testing/all-data', testingRouter);
 
-app.get("/",(req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   try {
-    res.status(ERRORS_CODE.OK_200).json("Server start!");
+    res.status(ERRORS_CODE.OK_200).json('Server start!');
   } catch (e) {
     res.status(ERRORS_CODE.INTERNAL_SERVER_ERROR_500).json(e);
   }
+});
