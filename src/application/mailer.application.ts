a3da6d4b1fea19e61fdbcccec8a07ec6291f1d78
dimaPolonez <dima-barxat@yandex.ru>
2023-01-22 14:38:26 +0300

    fix: hardcore auth

--- src/application/mailer.application.ts
+++ src/application/mailer.application.ts
@@ -8,8 +8,8 @@ class mailerApp {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: settings.MAIL_URL_USER,
-                pass: settings.MAIL_URL_PASS
+                user: 'testpolonez@gmail.com',
+                pass: 'gakzlzyovseopazk'
             }
         });
 
