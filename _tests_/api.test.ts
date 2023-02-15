9937dcc3ca5d55c46f4e7889ef7a71c628597cde
dimaPolonez <dima-barxat@yandex.ru>
2023-02-15 15:53:17 +0300

    fix tests

--- _tests_/api.test.ts
+++ _tests_/api.test.ts
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import request from 'supertest';
 import {app} from '../src';
 
@@ -9,6 +10,19 @@ describe('/', () => {
             .expect(200);
     });
 
+    it('blogs', async () => {
+        await request(app).get('/blogs/')
+            .expect(200);
+    });
 
+    it('blogs', async () => {
+        const response = await request(app).get("/blogs/");
+        expect(response.body).toHaveProperty("id");
+        expect(response.body).toHaveProperty("name");
+        expect(response.body).toHaveProperty("decription");
+        expect(response.body).toHaveProperty("websiteUrl");
+        expect(response.body).toHaveProperty("createdAt");
+        expect(response.statusCode).toBe(200);
+    });
 
 });
